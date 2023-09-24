14410b5b0f7ae4b1eaf1ee7acaddf632bbe8f6d2
vd-manh <110422164+vd-manh@users.noreply.github.com>
2023-09-24 19:14:20 +0700

    sua giao dien cua so chinh

--- menu.py
+++ menu.py
@@ -6,21 +6,21 @@ import trans
 import add
 
 gui_menu = Tk()
-# gui_menu.geometry('150x180')
+gui_menu.geometry('300x230')
 gui_menu.title('Menu')
 gui_menu.resizable(False, False)
-frame_menu = Frame(gui_menu, highlightbackground='black', highlightthickness=1)
+frame_menu = Frame(gui_menu,background = '#99FFFF' ,highlightbackground='black', highlightthickness=1)
 frame_menu.pack(fill=BOTH, expand=True, padx=5, pady=5)
 
 
 class Menu:
     def __init__(self):
-        label = Label(frame_menu, text="Choose Operation:", pady=10, font=('arial', 10, 'bold'))
+        label = Label(frame_menu,background = '#99FFFF', text="Chọn phép toán:", pady=10, font=('arial', 10, 'bold'))
 
-        inv = Button(frame_menu, text="Inverse", padx=30, pady=5, command=inverse.Inverse)
-        ad = Button(frame_menu, text="Add", padx=40, pady=5, command=add.Add)
-        tran = Button(frame_menu, text="Transpose", padx=22, pady=5, command=trans.Trans)
-        mlt = Button(frame_menu, text="Multiply", padx=28, pady=5, command=multi.Multi)
+        inv = Button(frame_menu, text="Phép nghịch đảo", padx=100, pady=5, command=inverse.Inverse)
+        ad = Button(frame_menu, text="Add", padx=140, pady=5, command=add.Add)
+        tran = Button(frame_menu, text="Phép chuyển đổi", padx=140, pady=5, command=trans.Trans)
+        mlt = Button(frame_menu, text="Phép nhân", padx=140, pady=5, command=multi.Multi)
 
         label.pack()
         inv.pack()
